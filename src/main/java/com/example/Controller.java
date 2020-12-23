9543274e0ecb5a61c8fb85b7110e0fab5517229f
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2020-12-22 16:34:59 -0800

    Simplifying the demo, adding sleuth

--- src/main/java/com/example/Controller.java
+++ src/main/java/com/example/Controller.java
@@ -22,21 +22,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.common.Foo1;
-
 /**
  * @author Gary Russell
  * @since 2.2.1
  */
 @RestController
 public class Controller {
-
-	@Autowired
-	private KafkaTemplate<Object, Object> template;
+	@Autowired private KafkaTemplate<Object, Object> template;
 
 	@PostMapping(path = "/send/foo/{what}")
 	public void sendFoo(@PathVariable String what) {
-		this.template.send("topic1", new Foo1(what));
+		this.template.send("topic1", what);
 	}
-
 }
